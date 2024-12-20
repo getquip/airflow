28bf6c99d4a051cb10e7ccc6b8291d45024fc028
Ammie Tea <ammie.tea@getquip.com>
2024-12-20 16:21:45 -0500

    update schema path, add chunk insert retries

--- dags/custom_packages/airbud/post_to_bigquery.py
+++ dags/custom_packages/airbud/post_to_bigquery.py
@@ -37,11 +37,11 @@ def create_table_if_not_exists(
         table_ref = client.dataset(dataset_name).table(endpoint)
         client.get_table(table_ref)
         print(f"Table '{endpoint}' already exists in dataset '{dataset_name}'.")
-
+    
     except Exception as e:
         print(f"Table '{endpoint}' does not exist'. Creating it now...")
         # Get the destination schema from the JSON file
-        schema = client.schema_from_json(f"data/{dataset_name}/{endpoint}.json")
+        schema = client.schema_from_json(f"clients/{dataset_name}/schemas/{endpoint}.json")
         
         # Create table object
         table = bigquery.Table(table_ref, schema=schema)
@@ -57,7 +57,7 @@ def create_table_if_not_exists(
             type_=partition_by,
             field=bigquery_metadata["partitioning_field"]
         )
-
+        
         # Set clustering
         table.clustering_fields = bigquery_metadata.get("clustering_fields", None)
         
@@ -103,21 +103,16 @@ def upload_to_bigquery(
 
     # Insert rows into BigQuery
     max_retries=3
-    for attempt in range(max_retries):
-        try:
-            # Insert data in chunks -- insert limit is 10,000 rows
-            chunk_size = 8000
-            for i in range(0, len(records), chunk_size):
-                chunk = records[i:i + chunk_size]
-                errors = client.insert_rows_json(table_ref, chunk)
-                if errors:
-                    print(f"Encountered errors while inserting rows: {errors}")
-                    break
-            print(f"Successfully inserted {len(records)} rows into {endpoint}.")
-            break
-        except Exception as e:
-            if attempt == max_retries - 1:
-                raise RuntimeError(f"Table {endpoint} not found after {max_retries} retries.")
-            print(f"An error occurred: {e}")
-            print(f"Retrying in 5 seconds...")
-            time.sleep(5)
+    # Insert data in chunks -- insert limit is 10,000 rows
+    chunk_size = 8000
+    for i in range(0, len(records), chunk_size):
+        for attempt in range(max_retries):
+            chunk = records[i:i + chunk_size]
+            errors = client.insert_rows_json(table_ref, chunk)
+            if errors:
+                print(f"Encountered errors while inserting rows: {errors}")
+                print(f"Retrying...")
+            else: # exit attempt loop if no errors
+                print("Successfully inserted chunk.")
+                break
+    print(f"Successfully inserted {len(records)} rows into {endpoint}.")
