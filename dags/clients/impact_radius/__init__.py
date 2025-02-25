da8489e84d19044dc186a96cd082e6d1e255de01
Ammie Tea <ammie.tea@getquip.com>
2025-02-25 13:55:35 -0500

    add schemas

---
+++ dags/clients/impact_radius/__init__.py
@@ -0,0 +1,185 @@
+# Standard package imports
+import json
+import pkgutil
+import logging
+import pandas as pd
+from typing import List, Dict
+
+# Third-party package imports
+from airflow.models import TaskInstance
+from google.cloud import storage, bigquery
+
+# Local package imports
+from custom_packages import airbud
+
+# Initialize logger
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
+log = logging.getLogger(__name__)
+
+class GetImactRadius(airbud.GetClient):
+    def __init__(self, project_id: str, bucket_name: str):
+        # Define client's dataset name
+        self.dataset = "impact_radius"
+
+        # Initialize the parent class
+        super().__init__(project_id, bucket_name, self.dataset) 
+
+        # Get API credentials
+        response = airbud.get_secrets(self.project_id, self.dataset, prefix="api__")
+        self.headers = {"Accept": "application/json"}
+        self.base_url = f"https://api.impact.com/Advertisers/{response.accountsid}/"
+
+        # Initialize BigQuery tables if they doesn't exist
+        self.endpoints = self.load_endpoints(f"{self.dataset}/endpoints.json")
+        self.schemas = self.load_endpoint_schemas(self.dataset, self.endpoints.keys())
+        self.generate_bq_tables()
+
+    def paginate_responses(
+        self,
+        endpoint: str,  # The API endpoint
+        endpoint_kwargs: dict,  # Endpoint-specific arguments
+        **kwargs,
+    ) -> List[Dict]:
+
+        # Set response limit to 250 (default is 50)
+        params = endpoint_kwargs.get("params", {})
+
+        # Get last bookmark, None if no bookmark
+        last_ts = airbud.get_last_page_from_last_dag_run(f"{self.dataset}__{endpoint}")
+        
+        # Logic to only fetch 1 day's worth of data (due to timeout issues)
+        if last_ts:
+            # Determine stop_at date
+            last_ts = pd.to_datetime(last_ts)
+            stop_at = last_ts + pd.Timedelta(days=1)
+            # If last_ts is today, do not pass the max parameter
+            current_date = pd.Timestamp.utcnow().normalize().date()
+            if last_ts.date() == current_date or stop_at.date() == current_date:
+                stop_at = None
+            # If stop_at date is today, do not pass the max parameter
+            elif stop_at.date() == current_date:
+                stop_at = None
+        else:
+            last_ts = pd.to_datetime('2024-06-20')
+            stop_at = pd.to_datetime('2024-06-21')
+
+        # Set the appropriate parameters for the endpoint
+        if endpoint == "events":
+            params["created_at_min"] = last_ts
+            if stop_at:
+                params["created_at_max"] = stop_at
+        else:
+            params["updated_at_min"] = last_ts
+            if stop_at:
+                params["updated_at_max"] = stop_at
+        log.info(f"Fetching data from {last_ts} to {stop_at if stop_at else 'now'}")
+
+        # Paginate through the API endpoint and create a list of records
+        url = self.base_url + endpoint
+        records = []
+        while True:
+            response = airbud.get_data(url, self.headers, params=params)
+            
+            # Check for Rate Limiting or other errors
+            if response.status_code != 200:
+                response = airbud.retry_get_data(url, self.headers, params=params)
+            if response.status_code == 200:
+                # Parse response for records and append to records list
+                response_json = response.json()
+                records.extend(response_json.get(endpoint))
+                
+                # Check if there is another page of data
+                next_page = response_json.get("next_cursor")
+                if next_page:
+                    # Only pass cursor as params
+                    log.info(f"Fetching next page of data...{next_page}")
+                    params = {"cursor": next_page}
+                else:
+                    log.info("No more data to fetch.")
+                    break
+            else:
+                log.info(f"Pagination halted due to status code: {response.status_code}")
+                break
+        
+        # Pass bookmark for next run
+        log.info(f"Finished with {len(records)} records.")
+        if len(records) > 0:
+            df = pd.DataFrame(records)
+            if endpoint == "events":
+                df_max = df["created_at"].max() 
+            else:
+                df_max = df["updated_at"].max()
+            if stop_at:
+                next_page = str(max(pd.to_datetime(df_max), stop_at))
+            else:
+                next_page = str(df_max)
+        else:
+            next_page = str(stop_at)
+        return records, next_page
+
+    def ingest_data(
+        self,
+        endpoint: str,  # API endpoint
+        endpoint_kwargs: dict,  # Endpoint-specific arguments
+        **kwargs
+        ) -> str:
+
+        # Get data
+        log.info(f"Ingesting data from {endpoint} endpoint.")
+        records, next_page = self.paginate_responses(endpoint, endpoint_kwargs, **kwargs)
+        log.info(f"Completed data fetch for {endpoint}")
+
+        # Store next page as XComs for downstream tasks
+        task_instance = kwargs['task_instance']
+        task_instance.xcom_push(key='next_page', value=next_page)
+        log.info(f"Stored next page for {endpoint} in XComs: {next_page}")
+
+        # Upload data to GCS
+        if len(records) > 0:
+            log.info(f"Uploading {len(records)} records to GCS...")
+            json_filename, dag_run_date = airbud.generate_json_blob_name(self.dataset, endpoint, **kwargs)
+            airbud.upload_json_to_gcs(self.gcs_bucket, json_filename, records)
+            return "success"
+        else:
+            # Store bookmark for next run
+            airbud.store_next_page_across_dags(self.dataset, endpoint, next_page)
+            return f"No records to upload."
+
+    def load_to_bq(
+        self,
+        endpoint: str,
+        endpoint_kwargs: dict,
+        **kwargs  # Additional keyword arguments
+        ) -> str:
+        # Check upstream task
+        task_instance = kwargs['ti']
+        upstream_task = f'get__{ endpoint }.ingest_{ endpoint }_data'
+        return_value = task_instance.xcom_pull(task_ids=upstream_task, key='return_value')
+
+        if return_value == "success":
+            try: # Get records from file or API
+                json_filename, dag_run_date = airbud.generate_json_blob_name(self.dataset, endpoint, **kwargs)
+                records = airbud.get_records_from_file(self.gcs_bucket, json_filename)
+                log.info(f"Successfully loaded {len(records)} records from GCS.")
+            except Exception as e:
+                raise Exception(f"Failed to get records from file or it doesn't exist: { e }")
+            
+            # Get BigQuery table destination
+            table_ref = self.bq_client.dataset(self.dataset).table(endpoint)
+
+            # Insert rows into BigQuery in chunks
+            chunk_size = endpoint_kwargs.get("chunk_size", 8000)
+            airbud.insert_records_to_bq(self.bq_client, table_ref, records, max_retries=3, chunk_size=chunk_size)
+            log.info(f"Successfully inserted { len(records) } rows into { endpoint }.")
+
+            # Get the next_page from XComs (from the upstream task)
+            task_instance = kwargs['ti']
+            upstream_task = f'get__{ endpoint }.ingest_{ endpoint }_data'
+            next_page = task_instance.xcom_pull(task_ids=upstream_task, key='next_page')
+
+            # Store bookmark for next run
+            airbud.store_next_page_across_dags(self.dataset, endpoint, next_page)
+
+        else:
+            log.info("Do Nothing.")
+
--- dags/clients/recharge/historical_backfill.py
+++ dags/clients/recharge/historical_backfill.py
@@ -5,16 +5,15 @@ from clients.recharge import GetRecharge
 
 
 project_id = 'quip-dw-raw'
-API_KEY = airbud.get_secrets(project_id, 'recharge', prefix="api__")
-RECHARGE_CLIENT = GetRecharge(API_KEY['api_key'])
+bucket_name = 'quip_airflow'
+RECHARGE_CLIENT = GetRecharge(project_id, bucket_name)
 
-endpoint = 'charges'
-url = f"{RECHARGE_CLIENT.base_url}/{endpoint}"
+endpoint = 'events'
+url = f"{RECHARGE_CLIENT.base_url}{endpoint}"
 headers = RECHARGE_CLIENT.headers
 params = {
 	"limit": 250,
-	"updated_at_min": "2024-06-20T00:00:00+00:00",
-	"updated_at_max": "2024-06-21T00:00:00+00:00"
+	"cursor": "eyJjcmVhdGVkX2F0X21pbiI6ICIyMDI0LTAyLTAxVDAwOjAwOjAwWiIsICJzdGFydGluZ19iZWZvcmVfaWQiOiA5NzQwNzk5MDIwLCAibGFzdF92YWx1ZSI6IDk3NDA3OTkwMjAsICJzb3J0X2J5IjogImlkLWRlc2MiLCAiY3Vyc29yX2RpciI6ICJuZXh0In"
 }
 records = []
 
@@ -25,11 +24,11 @@ while pd.to_datetime(params["updated_at_max"]) < stop_backfill:
     u_min= params["updated_at_min"]
     u_max= params["updated_at_max"]
     while True:
-        response = get_data(url, headers, params, None, None)
+        response = airbud.get_data(url, headers, params, None, None)
         
         # Check for Rate Limiting or other errors
         if response.status_code != 200:
-            response = retry_get_data(url, headers, params, None, None)
+            response = airbud.retry_get_data(url, headers, params, None, None)
         if response.status_code == 200:
             # Parse response for records and append to records list
             response_json = response.json()
@@ -82,6 +81,63 @@ airbud.insert_records_to_bq(bq_client, table_ref, records, max_retries=3, chunk_
 bucket = RECHARGE_CLIENT.gcs_bucket
 
 # Upload the JSON data as a string to GCS
-filename = f"get/recharge/{endpoint}/historical_to_2025.json"
+filename = f"get/recharge/{endpoint}/historical_072024.json"
 blob = bucket.blob(filename)
 blob.upload_from_string(json.dumps(records), content_type='application/json')
+
+
+cursors = {
+  #"01/01/25": "eyJjcmVhdGVkX2F0X21pbiI6ICIyMDI1LTAxLTAxVDAwOjAwOjAwWiIsICJzdGFydGluZ19iZWZvcmVfaWQiOiAxMzgzNDcwMDQ5NiwgImxhc3RfdmFsdWUiOiAxMzgzNDcwMDQ5NiwgInNvcnRfYnkiOiAiaWQtZGVzYyIsICJjdXJzb3JfZGlyIjogIm5leHQiLCAib2JqZWN0X3R5cGUiOiAic3Vic2NyaXB0aW9uIn0=0=",
+  #noevents "02/01/24": "eyJjcmVhdGVkX2F0X21pbiI6ICIyMDI0LTAyLTAxVDAwOjAwOjAwWiIsICJzdGFydGluZ19iZWZvcmVfaWQiOiA5Njg0Mjc3MjY5LCAibGFzdF92YWx1ZSI6IDk2ODQyNzcyNjksICJzb3J0X2J5IjogImlkLWRlc2MiLCAiY3Vyc29yX2RpciI6ICJuZXh0IiwgIm9iamVjdF90eXBlIjogInN1YnNjcmlwdGlvbiJ90=",
+  #"02/01/25": "eyJjcmVhdGVkX2F0X21pbiI6ICIyMDI1LTAyLTAxVDAwOjAwOjAwWiIsICJzdGFydGluZ19iZWZvcmVfaWQiOiAwLCAibGFzdF92YWx1ZSI6IDAsICJzb3J0X2J5IjogImlkLWRlc2MiLCAiY3Vyc29yX2RpciI6ICJuZXh0IiwgIm9iamVjdF90eXBlIjogInN1YnNjcmlwdGlvbiJ90=",
+  #noevents "03/01/24": "eyJjcmVhdGVkX2F0X21pbiI6ICIyMDI0LTAzLTAxVDAwOjAwOjAwWiIsICJzdGFydGluZ19iZWZvcmVfaWQiOiA5OTkzMzYyNzM1LCAibGFzdF92YWx1ZSI6IDk5OTMzNjI3MzUsICJzb3J0X2J5IjogImlkLWRlc2MiLCAiY3Vyc29yX2RpciI6ICJuZXh0IiwgIm9iamVjdF90eXBlIjogInN1YnNjcmlwdGlvbiJ90=",
+  #"04/01/24": "eyJjcmVhdGVkX2F0X21pbiI6ICIyMDI0LTA0LTAxVDAwOjAwOjAwWiIsICJzdGFydGluZ19iZWZvcmVfaWQiOiAxMDMyNTIyNTY0NSwgImxhc3RfdmFsdWUiOiAxMDMyNTIyNTY0NSwgInNvcnRfYnkiOiAiaWQtZGVzYyIsICJjdXJzb3JfZGlyIjogIm5leHQiLCAib2JqZWN0X3R5cGUiOiAic3Vic2NyaXB0aW9uIn0=",
+  #"05/01/24": "eyJjcmVhdGVkX2F0X21pbiI6ICIyMDI0LTA1LTAxVDAwOjAwOjAwWiIsICJzdGFydGluZ19iZWZvcmVfaWQiOiAxMDY4OTM1MzUyNCwgImxhc3RfdmFsdWUiOiAxMDY4OTM1MzUyNCwgInNvcnRfYnkiOiAiaWQtZGVzYyIsICJjdXJzb3JfZGlyIjogIm5leHQiLCAib2JqZWN0X3R5cGUiOiAic3Vic2NyaXB0aW9uIn0=",
+  #"06/01/24": "eyJjcmVhdGVkX2F0X21pbiI6ICIyMDI0LTA2LTAxVDAwOjAwOjAwWiIsICJzdGFydGluZ19iZWZvcmVfaWQiOiAxMTA2MjM0NDc0MCwgImxhc3RfdmFsdWUiOiAxMTA2MjM0NDc0MCwgInNvcnRfYnkiOiAiaWQtZGVzYyIsICJjdXJzb3JfZGlyIjogIm5leHQiLCAib2JqZWN0X3R5cGUiOiAic3Vic2NyaXB0aW9uIn0=",
+  #"07/01/24": "eyJjcmVhdGVkX2F0X21pbiI6ICIyMDI0LTA3LTAxVDAwOjAwOjAwWiIsICJzdGFydGluZ19iZWZvcmVfaWQiOiAxMTQyNzg5ODUzNSwgImxhc3RfdmFsdWUiOiAxMTQyNzg5ODUzNSwgInNvcnRfYnkiOiAiaWQtZGVzYyIsICJjdXJzb3JfZGlyIjogIm5leHQiLCAib2JqZWN0X3R5cGUiOiAic3Vic2NyaXB0aW9uIn0=",
+  #"08/01/24": "eyJjcmVhdGVkX2F0X21pbiI6ICIyMDI0LTA4LTAxVDAwOjAwOjAwWiIsICJzdGFydGluZ19iZWZvcmVfaWQiOiAxMTc4MDU2MDY0OCwgImxhc3RfdmFsdWUiOiAxMTc4MDU2MDY0OCwgInNvcnRfYnkiOiAiaWQtZGVzYyIsICJjdXJzb3JfZGlyIjogIm5leHQiLCAib2JqZWN0X3R5cGUiOiAic3Vic2NyaXB0aW9uIn0=",
+  #"09/01/24": "eyJjcmVhdGVkX2F0X21pbiI6ICIyMDI0LTA5LTAxVDAwOjAwOjAwWiIsICJzdGFydGluZ19iZWZvcmVfaWQiOiAxMjE0MTIzMjc1MiwgImxhc3RfdmFsdWUiOiAxMjE0MTIzMjc1MiwgInNvcnRfYnkiOiAiaWQtZGVzYyIsICJjdXJzb3JfZGlyIjogIm5leHQiLCAib2JqZWN0X3R5cGUiOiAic3Vic2NyaXB0aW9uIn0=",
+  #"10/01/24": "eyJjcmVhdGVkX2F0X21pbiI6ICIyMDI0LTEwLTAxVDAwOjAwOjAwWiIsICJzdGFydGluZ19iZWZvcmVfaWQiOiAxMjUyNTY5NjcwNiwgImxhc3RfdmFsdWUiOiAxMjUyNTY5NjcwNiwgInNvcnRfYnkiOiAiaWQtZGVzYyIsICJjdXJzb3JfZGlyIjogIm5leHQiLCAib2JqZWN0X3R5cGUiOiAic3Vic2NyaXB0aW9uIn0=",
+  #"11/01/24": "eyJjcmVhdGVkX2F0X21pbiI6ICIyMDI0LTExLTAxVDAwOjAwOjAwWiIsICJzdGFydGluZ19iZWZvcmVfaWQiOiAxMjk0NjY5MzAyMywgImxhc3RfdmFsdWUiOiAxMjk0NjY5MzAyMywgInNvcnRfYnkiOiAiaWQtZGVzYyIsICJjdXJzb3JfZGlyIjogIm5leHQiLCAib2JqZWN0X3R5cGUiOiAic3Vic2NyaXB0aW9uIn0=",
+}
+
+records = []
+
+cursor_date = "07/01/24"
+
+for cursor in cursors.values():
+    params = {
+        "limit": 250,
+        "cursor": cursors[cursor_date]
+    }
+    
+    while True:
+        response = airbud.get_data(url, headers, params, None, None)
+        
+        # Check for Rate Limiting or other errors
+        if response.status_code != 200:
+            response = airbud.retry_get_data(url, headers, params, None, None)
+        if response.status_code == 200:
+            # Parse response for records and append to records list
+            response_json = response.json()
+            records.extend(response_json.get(endpoint))
+            
+            # Check if there is another page of data
+            next_page = response_json.get("next_cursor")
+            if next_page:
+                # Only pass cursor as params
+                print(f"Fetching next page of data...{next_page}")
+                params = {"cursor": next_page}
+            else:
+                print("No more data to fetch.")
+                break
+        else:
+            log.error(f"Pagination halted due to status code: {response.status_code}")
+            break
+    
+    print(len(records))
+
+   df = pd.DataFrame(records)
+   df.dtypes
+   df.created_at.min()
+   df.created_at.max()
